b81ed19ba65e15d6dfd2605be1888df7c3091893
Robert Ferrão <robertferrao@gmail.com>
2025-10-07 21:08:23 -0300

    change module order declaration

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -31,19 +31,16 @@ module "blog_vpc" {
   }
 }
 
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.3"
 
+  vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
-  
-  target_group_arns = module.blog_alb.target_group_arns
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 module "blog_alb" {
@@ -78,14 +75,17 @@ module "blog_alb" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.3"
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
 
-  vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+  
+  target_group_arns = module.blog_alb.target_group_arns
 }
